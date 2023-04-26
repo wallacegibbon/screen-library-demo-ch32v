72ec4f1570e4b635cfd1cfdb96a09d315b9fa1ff
wallacegibbon <wallacegibbon@aliyun.com>
2023-04-27 00:07:28 +0800

    Trivial progress on OV2640: DVP initializing code is added and DVP interrupt code is not written yet.

--- src/camera_ov2640.c
+++ src/camera_ov2640.c
@@ -45,6 +45,19 @@
 /// SDA In
 #define SDA_IN_R (GPIOB->INDR & GPIO_Pin_11)
 
+//#define DVP_Work_Mode 0
+#define DVP_Work_Mode RGB565_MODE
+
+uintptr_t JPEG_DVPDMAaddr0 = 0x20005000;
+uintptr_t JPEG_DVPDMAaddr1 = 0x20005000 + OV2640_JPEG_WIDTH;
+
+uintptr_t RGB565_DVPDMAaddr0 = 0x2000A000;
+uintptr_t RGB565_DVPDMAaddr1 = 0x2000A000 + RGB565_COL_NUM * 2;
+
+volatile uint32_t frame_cnt = 0;
+volatile uint32_t addr_cnt = 0;
+volatile uint32_t href_cnt = 0;
+
 int SCCB_write_reg(uint8_t reg_addr, uint8_t reg_data);
 uint8_t SCCB_read_reg(uint8_t reg_addr);
 
@@ -532,6 +545,46 @@ void SCCB_gpio_initialize() {
 	IIC_SDA_SET;
 }
 
+static void DVP_initialize() {
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DVP, ENABLE);
+
+	DVP->CR0 &= ~RB_DVP_MSK_DAT_MOD;
+
+#if (DVP_Work_Mode == RGB565_MODE)
+	/// VSYNC、HSYNC:High level active
+	DVP->CR0 |= RB_DVP_D10_MOD | RB_DVP_V_POLAR;
+	DVP->CR1 &= ~(RB_DVP_ALL_CLR | RB_DVP_RCV_CLR);
+	DVP->ROW_NUM = RGB565_ROW_NUM;	// rows
+	DVP->COL_NUM = RGB565_COL_NUM * 2; // cols
+
+	DVP->DMA_BUF0 = RGB565_DVPDMAaddr0; // DMA addr0
+	DVP->DMA_BUF1 = RGB565_DVPDMAaddr1; // DMA addr1
+
+#endif
+
+	/// Set frame capture rate
+	DVP->CR1 &= ~RB_DVP_FCRC;
+	DVP->CR1 |= DVP_RATE_100P; // 100%
+
+	/// Interupt Enable
+	DVP->IER |= RB_DVP_IE_STP_FRM;
+	DVP->IER |= RB_DVP_IE_FIFO_OV;
+	DVP->IER |= RB_DVP_IE_FRM_DONE;
+	DVP->IER |= RB_DVP_IE_ROW_DONE;
+	DVP->IER |= RB_DVP_IE_STR_FRM;
+
+	NVIC_InitStructure.NVIC_IRQChannel = DVP_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	DVP->CR1 |= RB_DVP_DMA_EN; // enable DMA
+	DVP->CR0 |= RB_DVP_ENABLE; // enable DVP
+}
+
 void ov2640_send_command_table(const uint8_t *cmd_tbl, int size) {
 	int i;
 	for (i = 0; i < size / 2; i++)
@@ -542,6 +595,7 @@ int ov2640_initialize() {
 	int reg;
 
 	DVP_gpio_initialize();
+	DVP_initialize();
 	SCCB_gpio_initialize();
 
 	OV_PWDN_CLR;
@@ -573,6 +627,10 @@ int ov2640_initialize() {
 	);
 
 	return 0;
+}
+
+__attribute__((interrupt("WCH-Interrupt-fast")))
+void DVP_IRQHandler() {
 }
 
 void ov2640_JPEG_mode() {
