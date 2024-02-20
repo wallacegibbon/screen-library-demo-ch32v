56149d6c2931191b3c582bd48944973e141fceec
wallacegibbon <wallacegibbon@outlook.com>
2024-02-20 11:42:24 +0800

    Library upgrade: `screen-library-mcu` is upgraded. Errors with clangd got fixed.

--- src/main.c
+++ src/main.c
@@ -43,14 +43,14 @@ void fancy_display(struct painter *painter) {
 
 void initialize_screen_1(struct ssd1306_screen *screen, struct ssd1306_adaptor_ch32v_i2c *adaptor) {
 	ssd1306_adaptor_ch32v_i2c_initialize(adaptor, 0x3C);
-	ssd1306_initialize(screen, (const struct ssd1306_adaptor_i **)adaptor);
+	ssd1306_initialize(screen, (struct ssd1306_adaptor_i **)adaptor);
 	ssd1306_display_on(screen);
 }
 
 /*
 void initialize_screen_2(struct st7735_screen *screen, struct st7735_adaptor_ch32v_spi *adaptor) {
 	st7735_adaptor_ch32v_spi_initialize(adaptor, ...);
-	st7735_initialize(screen, (const struct st7735_adaptor_i **)adaptor);
+	st7735_initialize(screen, (struct st7735_adaptor_i **)adaptor);
 }
 */
 
@@ -102,7 +102,7 @@ void lcd_bg_set_brightness(uint16_t brightness) {
 void initialize_screen_3(struct st7789_screen *screen, struct st7789_adaptor_ch32v_fsmc *adaptor) {
 	st7789_adaptor_ch32v_fsmc_initialize(adaptor);
 
-	st7789_initialize(screen, (const struct st7789_adaptor_i **)adaptor);
+	st7789_initialize(screen, (struct st7789_adaptor_i **)adaptor);
 
 	// GPIO_SetBits(GPIOB, GPIO_Pin_14);
 
@@ -248,9 +248,9 @@ int main() {
 
 	// SSD1306_Screen_set_up_down_invert(&screen1);
 
-	// painter.drawing_board = (const struct drawing_i **)&screen1;
-	// painter.drawing_board = (const struct drawing_i **)&screen2;
-	painter.drawing_board = (const struct drawing_i **)&screen3;
+	// painter.drawing_board = (struct drawing_i **)&screen1;
+	// painter.drawing_board = (struct drawing_i **)&screen2;
+	painter.drawing_board = (struct drawing_i **)&screen3;
 
 	usart_printf_initialize(115200);
 	printf("System is ready now. SystemClk: %d\r\n", SystemCoreClock);
